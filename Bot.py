5ba9880cd0d558eb031f29ee71808c61c865658f
Azzamj0n <svjs7070@gmail.com>
2026-02-21 05:24:47 +0500

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("8228632825:AAFwJ9ZYJtj8DhmbO4wTA3CdiAtYhcDIkoM")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает 24/7 🚀")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен...")
+app.run_polling()
